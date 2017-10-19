53c76e26281fb93041af5012ebc60d5b169fedc8
Dell <Dell@192.168.2.16>
2017-10-18 21:43:13 -0400

    change path

--- src/formattted/output/to/text/file/FormatttedOutputToTextFile.java
+++ src/formattted/output/to/text/file/FormatttedOutputToTextFile.java
@@ -19,15 +19,16 @@ public class FormatttedOutputToTextFile {
      */
     public static void main(String[] args) 
     throws Exception{
-        Formatter out=new Formatter(new File("out.txt"));//Declare the formatter for the output
+        Formatter out=new Formatter(new File("C:\\Users\\Dell\\Downloads\\ZIED\\jas.txt"));//Declare the formatter for the output
         int grade1=89;
         double grade2=82;
         String name="Jaspreet Singh";
         double average=(grade1+grade2)/2;
         out.format("hello %s%n", name);//store the name in the output file
-        out.format("your grades are %d and %f", grade1,grade2);
-        out.format("The average of your grades is %f ", average);
-        
+        out.format("your grades are %d and %.2f%n", grade1,grade2);
+        out.format("The average of your grades is %.2f ", average);
+        out.close();
+        System.out.println("done");
     } 
     
 }
